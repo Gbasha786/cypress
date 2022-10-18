d48330367afdccf0fa47db88e55d53159c127689
Gbasha786 <97756779+Gbasha786@users.noreply.github.com>
2022-10-18 17:52:24 +0530

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,13 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser = {}, launchOptions) => {
+    console.log(launchOptions.args)
+
+    if (browser.name == 'chrome') {
+      launchOptions.args.push('--disable-gpu')
+    }
+
+    return launchOptions
+  })
 }
